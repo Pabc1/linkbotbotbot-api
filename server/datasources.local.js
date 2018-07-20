bd0c7abcbf525742f653061e237b5a446b380f27
Pablo Berganza <pberganza10@gmail.com>
2018-07-20 12:21:20 -0600

    ADDED: email datasource

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -5,4 +5,16 @@ module.exports = {
     connector: 'mongodb',
     name: 'mongoDs',
   },
+  emailDs: {
+    transports: [{
+      type: 'SMTP',
+      host: 'smtp.gmail.com',
+      secure: true,
+      port: 465,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
+      },
+    }],
+  },
 };
