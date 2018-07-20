d172f5c3809a091edbf6fe117d3f3963ac3eff2e
Pablo Berganza <pberganza10@gmail.com>
2018-07-19 18:50:03 -0600

    authentication for /links

--- common/models/link.js
+++ common/models/link.js
@@ -1,6 +1,22 @@
 'use strict';
+const app = require('../../server/server');
 
 module.exports = function(Link) {
+  Link.beforeRemote('find', (ctx, links, next) => {
+    console.log('hello');
+    const user = app.models.user;
+    user.findById(ctx.req.accessToken.userId, (err, result) => {
+      console.log(result);
+      ctx.args.filter = {
+        where: {
+          team: result.teamId,
+          user: result.slackId,
+        },
+      };
+      next();
+    });
+  });
+
   Link.afterRemote('find', (ctx, links, next) => {
     next();
   });
